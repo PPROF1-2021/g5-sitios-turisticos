f6bcb455f215780a4f5313b5f82559c60b8533b3
zarico87 <zarinarico@hotmail.com>
2021-11-15 00:00:39 -0300

    archivos ok-arreglando js

--- js/funcionesGrupo99.js
+++ js/funcionesGrupo99.js
@@ -1,13 +1,58 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }else{
+          alert("Datos enviados de manera exitosa")
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
+  function cargaBody(){
+    alert('En esta página podrás contactarte con el equipo ClickAr');
+  }
+
+function diferenciaDias(fecha){
+    var inicio = new Date().getTime();
+    var fin = new Date(fecha).getTime();
+    var dias = (fin-inicio)/(1000*60*60*24);
+
+    return Math.round(dias);
+
+}
+
+function cambiarColor(id,sombra){
+    document.getElementById(id).style.color = sombra;
+    console.log(document.getElementById(id).style.color);
+}
+
+function mostrarMensaje(msj){
+    alert(msj);
+}
+
 function obtenerdatos()
 {
-	var usuario=document.getElementById('usuario').value;
-	var contraseña=document.getElementById('contraseña').value;
+  var usuario=document.getElementById('usuario').value;
+  var contraseña=document.getElementById('contraseña').value;
 
-	document.write(" usuario: "+usuario+"<br> ");
-	document.write("contraseña: "+contraseña+"<br>");
+  document.write(" usuario: "+usuario+"<br> ");
+  document.write("contraseña: "+contraseña+"<br>");
 }
 
 setTimeout(function(obtenerdatos) {
-	location.href='index.html';
+  location.href='index.html';
 
 }, 1000 *10);
